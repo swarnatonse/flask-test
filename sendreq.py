9dc88253870401b589e618afd5c786727166510b
EC2 Default User <ec2-user@ip-172-31-7-186.ec2.internal>
2022-05-30 03:02:49 +0000

    test commit

--- sendreq.py
+++ sendreq.py
@@ -3,8 +3,8 @@ import json
 from aws_requests_auth.aws_auth import AWSRequestsAuth
 
 def create_request(credentials, endpoint, data):
-    auth = AWSRequestsAuth(aws_access_key=credentials.access_key,
-                       aws_secret_access_key=credentials.secret_key,
+    auth = AWSRequestsAuth(aws_access_key=credentials.get('access_key'),
+                       aws_secret_access_key=credentials.get('secret_key'),
                        aws_host=endpoint,
                        aws_region='us-east-1',
                        aws_service='lambda')
